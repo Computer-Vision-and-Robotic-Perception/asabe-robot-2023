8f4a51b01fc6e964f1d651a1202314ebcb05e8dc
ByronHernandez <bhernandezosorio@ufl.edu>
2023-06-24 05:38:23 -0400

    dynamixel_control - arduino bridge

---
+++ Navigation/new_tests/dynamixel_mirror/dynamixel_mirror.ino
@@ -0,0 +1,19 @@
+#include <Dynamixel2Arduino.h>
+#include <HardwareSerial.h>
+
+DYNAMIXEL::SerialPortHandler dynamixel_serial(Serial, 2);
+HardwareSerial& mirror_serial = Serial1;
+
+void setup() {
+  dynamixel_serial.begin();
+  mirror_serial.begin(57600);
+}
+
+void loop() {
+  if(mirror_serial.available()){
+    dynamixel_serial.write(mirror_serial.read());
+  }
+  if(dynamixel_serial.available()){
+    mirror_serial.write(mirror_serial.read());
+  }
+}
