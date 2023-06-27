9bfba7185469e2801ced16744990e317d7d566b8
ByronHernandez <bhernandezosorio@ufl.edu>
2023-06-27 18:30:00 -0400

    temporal solution to tf from diff_drive_controller

--- ROS/src/asabe_robot_description/launch/control_diff.launch.py
+++ ROS/src/asabe_robot_description/launch/control_diff.launch.py
@@ -2,7 +2,6 @@ from ament_index_python.packages import get_package_share_path
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import Command, LaunchConfiguration
 
 from launch_ros.actions import Node
@@ -10,23 +9,19 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
     urdf_tutorial_path = get_package_share_path('asabe_robot_description')
-    default_model_path = urdf_tutorial_path / 'urdf/base_description.urdf'
+    default_model_path = urdf_tutorial_path / 'urdf/robot_description_base.urdf'
     default_rviz_config_path = urdf_tutorial_path / 'rviz/urdf.rviz'
-    # BH Add
-    controller_config = urdf_tutorial_path / 'config/controllers.yaml' 
-    # BH End
+    controller_config = urdf_tutorial_path / 'config/control_diff.yaml' 
 
-    gui_arg = DeclareLaunchArgument(name='gui', default_value='false', choices=['true', 'false'],
-                                    description='Flag to enable joint_state_publisher_gui')
     model_arg = DeclareLaunchArgument(name='model', default_value=str(default_model_path),
                                       description='Absolute path to robot urdf file')
+    
     rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
                                      description='Absolute path to rviz config file')
 
     robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
                                        value_type=str)
 
-    # BH Add
     controller_manager = Node(
             package="controller_manager",
             executable="ros2_control_node",
@@ -49,25 +44,11 @@ def generate_launch_description():
             output="screen",
         )
 
-    # BH End
-
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{'robot_description': robot_description}]
     )
-
-    # joint_state_publisher_node = Node(
-    #     package='joint_state_publisher',
-    #     executable='joint_state_publisher',
-    #     condition=UnlessCondition(LaunchConfiguration('gui'))
-    # )
-
-    # joint_state_publisher_gui_node = Node(
-    #     package='joint_state_publisher_gui',
-    #     executable='joint_state_publisher_gui',
-    #     condition=IfCondition(LaunchConfiguration('gui'))
-    # )
 
     rviz_node = Node(
         package='rviz2',
@@ -78,17 +59,11 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        gui_arg,
         model_arg,
         rviz_arg,
-        # BH Add
         controller_manager,
         joint_state_broadcaster,
         diff_drive_controller,
-        # BH End
-        # joint_state_publisher_node,
-        # joint_state_publisher_gui_node,
         robot_state_publisher_node,
         rviz_node
     ])
-
--- ROS/src/asabe_robot_description/launch/gazebo.launch.py
+++ ROS/src/asabe_robot_description/launch/gazebo.launch.py
@@ -13,7 +13,7 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     package_name = 'asabe_robot_description'
     world_file_name = 'empty_world.world'
-    robot_name = 'robot_description.urdf'
+    robot_name = 'robot_description_full.urdf'
 
     world = os.path.join(get_package_share_directory(
         package_name), 'world', world_file_name)
