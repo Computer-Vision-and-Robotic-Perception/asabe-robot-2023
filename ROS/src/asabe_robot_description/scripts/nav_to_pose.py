ff4badcce9614df1b89569b721877d496dbc9ca3
ByronHernandez <bhernandezosorio@ufl.edu>
2023-07-11 13:48:09 -0400

    update

--- ROS/src/asabe_robot_description/scripts/nav_to_pose.py
+++ ROS/src/asabe_robot_description/scripts/nav_to_pose.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
+# Copyright 2021 Samsung Research America
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Modified by AutomaticAddison.com
  
 import time  # Time library
-
+ 
 from geometry_msgs.msg import PoseStamped # Pose with ref frame and timestamp
 from rclpy.duration import Duration # Handles time for ROS 2
 import rclpy # Python client library for ROS 2
@@ -49,9 +64,19 @@ def main():
   # local_costmap = navigator.getLocalCostmap()
 
   pose_list = [[2.20, -0.3, 0.0, -1.0],
-               [2.20, -0.8, 0.0, -1.0],
-               [2.20, -0.8, 0.0, 0.0],
-               [0.5, -0.8, 0.0, 0.0]]
+               [2.20, -0.9, 0.0, -1.0],
+               [2.20, -0.9, 0.0, 0.0],
+               [1.72, -0.9, 0.0, 0.0],
+               [1.60, -0.9, 0.0, 0.0],
+               [1.48, -0.9, 0.0, 0.0],
+               [1.36, -0.9, 0.0, 0.0],
+               [1.24, -0.9, 0.0, 0.0],
+               [1.12, -0.9, 0.0, 0.0],
+               [1.00, -0.9, 0.0, 0.0],
+               [0.88, -0.9, 0.0, 0.0],
+               [0.76, -0.9, 0.0, 0.0],
+               [0.5, -0.9, 0.0, 0.0],
+               [0.5, -0.9, 0.0, 1.0]]
  
   for p in pose_list:
   
